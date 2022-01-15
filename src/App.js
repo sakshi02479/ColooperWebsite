0a61a56fd276087d822aa48c3c66cfab915799aa
sakshirawat0247 <sakshi.rawat@telusinternational.com>
2022-01-15 20:03:10 +0530

    first initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import logo_animation from './Logo_Animation_1.mp4';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><div className="Video_top">
+      <video autoPlay muted loop id="myVideo">
+        <source src={logo_animation} title="Colooper" type="video/mp4" />
+      </video>
+    </div><div className="content">
+        <h1>Our site is coming soon!</h1>
+      </div><div className="social-container footer">
+        <h4>Get in touch with us</h4>
+        <ul className="social-icons" style={{ marginBottom: 5 }}>
+          <li><a href="https://www.facebook.com/colooperglobal"><i title="facebook" className="fa fa-facebook"></i></a></li>
+          <li><a href="https://www.fa-instagram.com/colooperglobal"><i title="instagram" className="fa fa-instagram"></i></a></li>
+          <li><a href="https://www.twitter.com/colooperglobal"><i title="twitter" className="fa fa-twitter"></i></a></li>
+          <li><a href="https://www.linkedin.com/company/colooper"><i title="linkedin" className="fa fa-linkedin"></i></a></li>
+          <li><a href="mailto:sales@colooper.com?subject = Quote Request for a New Idea&body = Message"><i title="Email" className="fa fa-envelope"></i></a></li>
+        </ul>
+        <h5 style={{ marginTop: 0, marginBottom: 10 }}>&copy; 2022 Colooper.All rights reserved</h5>
+      </div></>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 ReactDOM.render(
   <React.StrictMode>
